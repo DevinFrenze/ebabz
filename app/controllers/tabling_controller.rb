f89f8541d12ff70c34a193d124b3300f049bf080
Devin Frenze <devinfrenze@Devins-MBP.att.net>
2017-08-19 03:06:18 -0700

    setup routes

--- app/controllers/tabling_controller.rb
+++
@@ -1,2 +0,0 @@
-class TablingController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'about',    to: "about#index"
-  get 'tabling',  to: "tabling#index"
+  get 'about',      to: "about#index"
+  get 'apply',      to: "apply#index"
+  get 'history',    to: "history#index"
+  get 'vendors',    to: "vendors#index"
+  get 'volunteer',  to: "about#index"
 
   root 'home#index'
 end
