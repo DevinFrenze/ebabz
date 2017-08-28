f2e18549fda57df0d5f117a2d031906186b954bc
Devin Frenze <devinfrenze@Devins-MBP.att.net>
2017-08-27 18:18:17 -0700

    redid home page layout, links might be broken on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :set_fest
+  layout 'basic_page'
 
   def set_fest
     @fest = Fest.last || Fest.new
