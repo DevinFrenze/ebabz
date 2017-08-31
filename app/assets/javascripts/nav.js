fc360c4cd8dbedfa732280f2dc297d5c26d9424d
Devin Frenze <devinfrenze@Devins-MBP.att.net>
2017-08-31 00:13:39 -0700

    header transition ALMOST done

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   nav = $("#nav");
   threshold = nav.offset().top;
   fixedClass = "m--fixed";
+  rightClass = "m--right";
 
   $(window).scroll(function() {
     nav = $("#nav");
@@ -9,11 +10,15 @@ $(document).ready(function() {
 
     if ($(window).scrollTop() >= threshold) {
       if (!nav.hasClass(fixedClass)) {
-        nav.addClass(fixedClass);
+        nav.addClass(fixedClass).delay(250).queue(function() {
+          nav.addClass(rightClass).dequeue();
+        });
         header.css("paddingBottom", "" + nav.outerHeight() + "px");
       }
     } else if (nav.hasClass(fixedClass)) {
-      nav.removeClass(fixedClass);
+      nav.removeClass(fixedClass).delay(250).queue(function() {
+        nav.removeClass(rightClass).dequeue();
+      });
       header.css("paddingBottom", "0px");
     }
   });
