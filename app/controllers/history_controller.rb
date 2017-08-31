6fcecaa6480a0e67681a8f5b6de17b9ddfe6072e
Devin Frenze <devinfrenze@Devins-MacBook-Pro.local>
2017-08-31 16:57:21 -0700

    fixed link styles and added some colors

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,7 +1,7 @@
 class HistoryController < ApplicationController
   def index
     @general_info = GeneralInfo.last || GeneralInfo.new
-    @articles = Article.all.sort_by {|a| a.year}
+    @articles = Article.all.sort_by {|a| a.year}.reverse
     @past_fests = Fest.where.not(id: @fest.id)
   end
 end
